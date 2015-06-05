874ca868c1e8f2fd134a987f3758db4fdef66fc3
subburam <subburam@Ratel-Ultra.(none)>
2015-06-04 21:26:02 -0400

    added documentation.

--- neighbor.py
+++ neighbor.py
@@ -3,16 +3,34 @@ Implement some nearest neighbors statistics.
 """
 from __future__ import division
 import numpy as np
-import tamarind.stats
+import stats as T_stats
 
 def reciprocal_weights(distances, scale=1, power=1):
-
+    """
+    Function to generate weights that tail off for more distant points.
+    """
+    
     return (1 / (1 + distances*scale))**power
 
 
 def stats(response, distance_matrix, knn, weights=None, cutoff=None,
           reciprocal=None):
+    """
+    Return for each element of response:
+    a) stats of distances of knn nearest neighbors,
+    b) stats of response among knn nearest neighbors, and
+    c) list of the knn nearest neighbors, subject to cutoff.
 
+    weights:
+    return weighted stats with these as weights of the neighbors.
+
+    cutoff:
+    ignore neighbors further than this, even if within knn nearest.
+
+    reciprocal:
+    weight further neighbors less, calling reciprocal_weights().
+    """
+    
     nearest = np.argsort(distance_matrix, axis=1)[:, 1:]
     r_stats, d_stats, neighbors = [], [], []
     for count, row in enumerate(nearest):
@@ -35,15 +53,19 @@ def stats(response, distance_matrix, knn, weights=None, cutoff=None,
         else: w = None
 
         
-        d_stats.append(tamarind.stats.Full.stats(distances, weights=w))
-        r_stats.append(tamarind.stats.Full.stats(response[row[:len(distances)]], weights=w))
+        d_stats.append(T_stats.Full.stats(distances, weights=w))
+        r_stats.append(T_stats.Full.stats(response[row[:len(distances)]], weights=w))
 
-    return tamarind.stats.Datab(d_stats, process_Nones=True), \
-           tamarind.stats.Datab(r_stats, process_Nones=True), neighbors
+    return T_stats.Datab(d_stats, process_Nones=True), \
+           T_stats.Datab(r_stats, process_Nones=True), neighbors
 
 
 def distance_matrix(features, power=2, weights=None):
-
+    """
+    Compute Euclidean distance between feature vectors, optionally weighting
+    each dimension.
+    """
+    
     recip = 1.0 / power
     if weights is not None:
         features = np.array(weights)[np.newaxis, :] / np.sum(weights) * features
