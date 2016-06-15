24f1e5c211dbbc32d6dde461d8442d89de80fdd1
Deepak Subburam <deepak@subburam.org>
2016-06-14 20:41:10 -0400

    standard distutils setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='tamarind',
+      version='0.2',
+      description='House, examine and model panel data.',
+      author='Deepak Subburam',
+      author_email='deepak@subburam.org',
+      url='https://github.com/Fenugreek/tamarind',
+      packages=['tamarind'],
+      scripts=[],
+      license="GNU GPL v3")
