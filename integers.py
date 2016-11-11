5c3d093eb8a0bab4d049a2b4cf97feb151f30b43
Deepak Subburam <deepak@subburam.org>
2016-11-11 16:31:25 -0500

    New tile option to squarest_factors.

--- integers.py
+++ integers.py
@@ -9,19 +9,20 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 """
 
-def squarest_factors(integer):
+def squarest_factors(integer, tile=[1, 1]):
     """
     Return the two factors A, B of given positive integer I such that
         A * B = I
-    and |A - B| is as small as possible.
+    and |A*a - B*b| is as small as possible.
 
     Return (1, integer) if integer is prime.
     """
 
     if integer < 1: raise ValueError('Input must be positive integer.')
 
-    test = int(integer**.5)
-    while test > 1 and integer % test:
-        test -= 1
+    area = integer * tile[0] * tile[1]
+    rows = int(area**.5 / tile[0])
+    while rows > 1 and integer % rows:
+        rows -= 1
 
-    return test, integer / test
+    return rows, integer / rows
