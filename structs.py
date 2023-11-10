d6ea399a0e56a2a0814271608095d05d875b0576
Deepak Subburam <dsubburam@gmail.com>
2023-11-10 12:46:12 -0500

    For data structures missing from standard python.

---
+++ structs.py
@@ -0,0 +1,63 @@
+"""
+Some useful data structures missing from standard python.
+"""
+
+class Trie:
+    """
+    Implement a prefix tree.
+    
+    Adapted from https://stackoverflow.com/questions/11015320/how-to-create-a-trie-in-python/11015381#11015381
+    """
+
+    def __init__(self, sequences, store=False, _end='_end_'):
+        """
+        Given a list of lists, construct and store a prefix tree.
+        The leaves are 2-tuples of the form (depth, sequence_index).
+
+        store: store the given sequences as self.sequences.
+        
+        _end: use this as the key for the leaves of the tree.
+        """
+        self._end = _end
+        if store:
+            self.sequences = sequences
+        
+        root = dict()
+        for index, sequence in enumerate(sequences):
+            current_dict = root
+            for depth, token in enumerate(sequence):
+                current_dict = current_dict.setdefault(token, {})
+            current_dict[_end] = (depth, index)
+
+        self.trie = root
+        
+
+    def contains(self, sequence):
+        """Returns whether given sequence is in the prefix tree."""
+        
+        current_dict = self.trie
+        for token in sequence:
+            if token not in current_dict:
+                return False
+            current_dict = current_dict[token]
+        return self._end in current_dict
+
+
+    def match(self, sequence):
+        """
+        Returns number of elements into the given sequence that traverse the
+        prefix tree successfully, and
+        a) the leaf if that traversal gets to a leaf, or
+        b) None if otherwise.
+        
+        Result will be (<depth>, None|<leaf>), where
+        0 <= depth <= len(sequence).
+        """
+        count, current_dict = 0, self.trie
+        for token in sequence:
+            if token not in current_dict:
+                break
+            current_dict = current_dict[token]
+            count += 1
+        return count, current_dict.get(self._end, None)
+    
